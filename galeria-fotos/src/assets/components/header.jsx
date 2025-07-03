3349f19119f73b2d6d9cc9a473921f489aad79df
Daniel <isral.tudela712.96@gmail.com>
2025-07-03 19:18:17 -0400

    practica calificada 2

--- galeria-fotos/src/assets/components/header.jsx
+++ galeria-fotos/src/assets/components/header.jsx
@@ -11,28 +11,28 @@ export default function Header({ onBuscar }) {
   };
 
   return (
-    <div className="text-center">
+    <div className="text-center mt-30">
       <h1 className="text-4xl font-bold mt-6 mb-6">SnapShot</h1>
 
       <form onSubmit={handleSubmit} className="flex justify-center mb-4">
         <input
           type="text"
-          placeholder="Buscar imágenes..."
-          className="p-2 w-1/2 rounded-l"
+          placeholder="Search..."
+          className="p-2 w-1/2 rounded-l max-w-xl bg-gray-200 rounded shadow"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button type="submit" className="bg-blue-500 text-white px-4 rounded-r">
+        <button type="submit" className="bg-blue-500 text-white px-4">
           🔍
         </button>
       </form>
 
-      <div className="flex flex-wrap justify-center gap-2 mb-6">
+      <div className="flex flex-wrap justify-center gap-6 mb-6">
         {["Mountains", "Beaches", "Birds", "Food"].map((cat) => (
           <button
             key={cat}
             onClick={() => onBuscar(cat)}
-            className="bg-gray-200 px-6 py-2 rounded-full w-48"
+            className="bg-blue-900 hover:bg-blue-700 text-white px-4 py-2  w-28 h-8 text-sm font-semibold shadow"
           >
             {cat}
           </button>
--- galeria-fotos/src/assets/components/imageGrid.jsx
+++ galeria-fotos/src/assets/components/imageGrid.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function ImageGrid({ images }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {images.map((img) => (
-        <img
-          key={img.id}
-          src={img.src.medium}
-          alt={img.photographer}
-          className="rounded shadow w-full h-52 object-cover"
-        />
-      ))}
+    <div className="px-4 sm:px-8 lg:px-16">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {images.map((img) => (
+          <img
+            key={img.id}
+            src={img.src.medium}
+            alt={img.photographer}
+            className="rounded shadow w-full h-36 object-cover"
+          />
+        ))}
+      </div>
     </div>
   );
 }
